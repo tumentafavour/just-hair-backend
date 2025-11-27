e6ade69597e730fd333e72b71722541d6b5af4bc
BebsYanski <bebongnchuy@gmail.com>
2025-11-27 15:32:13 +0100

    increased rate limit

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 
-limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])  # no app yet
+limiter = Limiter(key_func=get_remote_address, default_limits=["500 per day", "100 per hour"])  # no app yet
 
 db = SQLAlchemy()
 migrate = Migrate()
